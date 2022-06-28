6b526d35ed53e83910e6527892b61a8757ff3e48
Emicla <emiclalenondarosa@gmail.com>
2022-06-28 19:06:29 -0300

    Prevenindo possíveis erros

--- script.js
+++ script.js
@@ -5,28 +5,26 @@ let tituloTabuada = document.getElementById("numero-tabuada");
 let num1 = "";
 let num2 = "";
 let operador = "+";
-let verifica = false;
+let escolheuNum1 = false;
 
 function digita(valor) {
     var tela = visor.textContent;
     var teclaClicada = valor.textContent;
 
-    if (String(parseInt(num1)) == "NaN" && tela.length > 0) {
-        num1 = "0"
-        visor.innerHTML = num1;
-        console.log("Não é um número")
-    }
-
-    if (verifica && teclaClicada != "+" && teclaClicada != "-" && teclaClicada != "x" && teclaClicada != "/") {
+    if (escolheuNum1 && teclaClicada != "+" && teclaClicada != "-" && teclaClicada != "x" && teclaClicada != "/") {
         visor.innerHTML += valor.textContent;
 
-    } else if (verifica == false) {
+    } else if (escolheuNum1 == false && String(parseInt(num1)) == "NaN" && tela.length > 0) {
+        num1 = "0"
+        visor.innerHTML = num1 + valor.textContent;
+
+    } else if (escolheuNum1 == false) {
         visor.innerHTML += valor.textContent;
     }
 }
 
 function addNumero(numero) {
-    if (verifica) {
+    if (escolheuNum1) {
         num2 += numero.textContent;
     } else {
         num1 += numero.textContent;
@@ -34,18 +32,18 @@ function addNumero(numero) {
 }
 
 function selecOperacao(operacao) {
-    if (verifica == false) {
+    if (escolheuNum1 == false) {
         operador = operacao.textContent;
-        verifica = true;
+        escolheuNum1 = true;
     }
 }
 
 function apagarLetra() {
-    if (verifica == false) {
+    if (escolheuNum1 == false) {
         visor.textContent = visor.textContent.substring(0, visor.textContent.length - 1);
         num1 = num1.substring(0, num1.length - 1);
 
-    } else if (verifica && num2 != "") {
+    } else if (escolheuNum1 && num2 != "") {
         num2 = num2.substring(0, num2.length - 1);
         visor.textContent = num1 + operador + num2;
     }
@@ -57,7 +55,7 @@ function apagarTudo() {
     operador = "+";
     num1 = "";
     num2 = "";
-    verifica = false;
+    escolheuNum1 = false;
 }
 
 function calcula() {
@@ -91,14 +89,14 @@ function calcula() {
     num2 = "";
     num1 = resultado.toString();
     operador = "+";
-    verifica = false;
+    escolheuNum1 = false;
 }
 
 function calcTabuada() {
     tabuada.innerHTML = "";
     divTabuada.classList.remove("display-none");
     tituloTabuada.innerHTML = "Tabuada do " + num1;
-    if (verifica == false && num1 != "") {
+    if (escolheuNum1 == false && num1 != "") {
         for (var i = 0; i <= 10; i++) {
             tabuada.innerHTML += "<li>" + num1 + " x " + i + " = " + (num1 * i) + "</li>";
         }
